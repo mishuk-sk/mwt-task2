91155feda9eaba332875b704a24e263b230c2537
Michael Skuratovich <mishuk.sk@gmail.com>
2022-01-10 21:40:18 +0300

    fixed tfc

--- main.js
+++ main.js
@@ -23,11 +23,11 @@ app.get("/login/2", (req, resp)=>{
     resp.send("Skuratovich");
 })
 app.get("/login/code1", (req, resp)=>{
-    resp.send('<html><body><center><p style="font-size: 21px; font-weight:900">Your family</p></center></body></html>');
+    resp.send('<html><body><center><p style="font-size: 21px; font-weight:900">Skuratovich`s</p></center></body></html>');
 })
 app.get("/login/code2", (req, resp)=>{
-    resp.set('Content-Type', "text/html");
-    resp.send('<html><body><center><p style="font-size: 21px; font-weight:900">Your family</p></center></body></html>');
+    resp.set('Content-Type', "text/plain");
+    resp.send('<html><body><center><p style="font-size: 21px; font-weight:900">Skuratovich`s</p></center></body></html>');
 })
 app.get("/promise", (req, resp) => {
     resp.sendFile(path.resolve(__dirname, 'promise.js'));
